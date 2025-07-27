bcb80d2211ad6631a9b76674dd5ffab0e6fcaac3
Tsogt Enkhbat <tsogt.enkhbat@gmail.com>
2025-07-27 16:30:49 -0600

    Update

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -34,9 +34,9 @@ const Home = () => {
         return;
       }
       setTweets((prevTweets) => [...prevTweets, ...data]);
-    } catch (error) {}
+    } catch (error) { }
   };
-  
+
   useEffect(() => {
     fetchTweets(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -44,41 +44,54 @@ const Home = () => {
 
   useLayoutEffect(() => {
     const contentHeight = contentRef.current.clientHeight;
-    if(contentHeight < window.innerHeight) {
+    if (contentHeight < window.innerHeight) {
       setPage(2)
     }
   }, []);
 
   const refetchTweets = (username, tweet) => {
-    const newTweet = { username: username, tweet: tweet};
+    const newTweet = { username: username, tweet: tweet };
     setTweets((prevList) => [newTweet, ...prevList]);
   };
 
 
   return (
     <Layout>
-      <div id="tweet-main-content" className="tweet-main-content">
-        <div ref={contentRef}>
+      <div id="tweet-main-content" className="tweet-main-content bg-gray-100 min-h-screen py-6">
+        <div
+          ref={contentRef}
+          className="max-w-2xl mx-auto bg-white rounded-2xl shadow-lg p-4 space-y-6"
+        >
+          <h2 className="text-2xl font-semibold text-center text-gray-800">Tweet Feed</h2>
+
           <TweetInput refetchTweets={refetchTweets} />
+
           <InfiniteScroll
             dataLength={tweets.length}
             next={() => setPage((page) => page + 1)}
             hasMore={hasMore}
-            loader={<h4>...</h4>}
-            scrollableTarget={"tweet-main-content"}
+            loader={
+              <div className="flex justify-center my-4 text-gray-500">
+                <span className="animate-pulse">Loading more tweets...</span>
+              </div>
+            }
+            scrollableTarget="tweet-main-content"
           >
-            {tweets.map((tweet, index) => (
-              <Tweet
-                key={index}
-                tweet={tweet.tweet}
-                username={tweet.username}
-              />
-            ))}
+            <div className="space-y-4">
+              {tweets.map((tweet, index) => (
+                <Tweet
+                  key={index}
+                  tweet={tweet.tweet}
+                  username={tweet.username}
+                />
+              ))}
+            </div>
           </InfiniteScroll>
         </div>
       </div>
     </Layout>
   );
+
 };
 
 export default Home;
